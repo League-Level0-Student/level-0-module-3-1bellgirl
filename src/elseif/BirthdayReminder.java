0302c6f6c7d14e46a8bf0d2cc8f767c9820f8c2a
league <league@wts-im-17.attlocal.net>
2018-03-28 19:56:04 -0700

    BananaQuiz BirthdayReminder DizzyRobot ShinyObjects

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -11,21 +11,30 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
-
+		String momsBirthday = "November 14th";
+		String dadsBirthday = "January 24th";
+		String myBirthday = "May 31st";
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	String reminder= JOptionPane.showInputDialog("Which birthday do you want to be reminded of?");
 		// 3. Print out what the user typed
-		
 		// 4. if user asked for "mom"
 			//print mom's birthday
+		if(reminder.equals("mom")) {
+			JOptionPane.showMessageDialog(null, "November 14");
+		}
 		// 5. if user asked for "dad"
 			// print dad's birthday
+		else if(reminder.equals("dad")) {
+			JOptionPane.showMessageDialog(null, "January 24th");
+		}
 		// 6. if user asked for your name
 			// print myBirthday
+		else if (reminder.equals("maclaryn")){
+			JOptionPane.showMessageDialog(null, "May 31st");
+		}
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+		else {
+			JOptionPane.showMessageDialog(null, "Sorry, I don't remember that person's birthday!");
+		}
 	} 
 }
